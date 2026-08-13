1fb12df507669129d00dcce5467bb8de35f1bc31
rafark <rafarks@gmail.com>
2026-08-12 22:26:34 -0600

    Added warning if installed bin is not in your path

--- bin/install.sh
+++ bin/install.sh
@@ -9,3 +9,8 @@ mkdir -p "$targetDir"
 curl -sSL https://github.com/neblabs/header-injector/releases/latest/download/header-injector.phar -o "$targetFilePath"
 
 sudo chmod +x "$targetFilePath"
+
+# warn if not in path
+if ! [[ "$targetDir" == *"/.local/bin"* ]]; then
+    echo [warn] Installed to "$targetFilePath" but it "doesn't" seem to be in your PATH.
+fi
